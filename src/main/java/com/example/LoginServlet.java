ac33421337064383770814f09b8ea0b42baa5d68
rishikeshvijay1999 <144434263+rishikeshvijay1999@users.noreply.github.com>
2024-02-28 12:53:34 +0530

    Update LoginServlet.java

--- src/main/java/com/example/LoginServlet.java
+++ src/main/java/com/example/LoginServlet.java
@@ -17,9 +17,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     // JDBC connection parameters
-    private static final String JDBC_URL = "jdbc:mysql://192.168.138.114:3306/myDB";
+    private static final String JDBC_URL = "jdbc:mysql://192.168.75.157:3306/mydb";
     private static final String JDBC_USER = "mysql";
-    private static final String JDBC_PASSWORD = "mysql";
+    private static final String JDBC_PASSWORD = "mysql@123";
 
     // Declare the connection variable at the class level
     private Connection connection;
